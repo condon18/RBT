a2574bdcff0d8a1123e6ba8cf2eebba0271f20c2
dehart18 <dehart18@up.edu>
2017-04-03 16:31:58 -0700

    latest version

--- main.cpp
+++ main.cpp
@@ -1,10 +1,20 @@
 #include "rbtree.h"
+#include <iostream>
+
+using namespace std;
 
 int main(int argc, char* argv[])
 {
 
   RBTree theTree;
   
+  theTree.RBinsert(1);
+  
+  theTree.RBinsert(2);
+  
+  //theTree.RBinsert(3);
+  
+  //theTree.Traverse();
 
   return 0;
 }
